36881070a2bc74d69b397eeabef5127685f9dc14
ilhanu <ilhanu@users.noreply.github.com>
2024-01-19 20:08:16 +0000

    jekyll build from Action 38c730f17067ef258ff1b686b885e78a7bf9468f

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Invitation to test FS",
+        "excerpt":"Invitation for Housing Europe partners who want to co-develop social innovations What is Fair Squares Fair Squares is committed to let the number of affordable rental houses grow, motivated by the unfair lack of adequate housing. This urgent societal problem we face impacts a huge number of people, also in...","categories": [],
+        "tags": [],
+        "url": "https://github.com/pages/Fair-Squares/website/2024/01/19/invitation.html"
+      }]
